550aecd83fb8933b0abbbdba1bb78aa2040d5b88
Yunix <97799691+TheRealYunix@users.noreply.github.com>
2022-01-28 01:08:24 +0100

    Update Window's Activator.bat

--- Window's Activator.bat
+++ Window's Activator.bat
@@ -7,6 +7,8 @@ slmgr.vbs /ckms
 slmgr /skms localhost
  
 slmgr /ipk GCRJD-8NW9H-F2CDX-CCM8D-9D6T9
+::Example for Windows 8.1 Pro Replace "GCRJD-8NW9H-F2CDX-CCM8D-9D6T9" with a key from Google.
+
 slmgr /skms kms.digiboy.ir
 slmgr /ato
 
